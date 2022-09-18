e5d3c7b40cb887196da4db4fa617fb11b0801001
R Anantha Krishnan <anantha.raghunath@gmail.com>
2022-09-18 15:57:11 +0530

    error handling

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const app = require('./app');
 const mongoose = require('mongoose');
-const PORT = 8080;
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 8080;
 
-mongoose.connect('mongodb+srv://Anantha:Anantha@cluster0.srehusp.mongodb.net/?retryWrites=true&w=majority');
 
-mongoose.connection.once('open', () => {
-    console.log('connection established')
-}).on('connectionError', (err) => {
-    console.log(err);
+// mongoose.connect('mongodb+srv://Anantha:Anantha@cluster0.nuxbelc.mongodb.net/?retryWrites=true&w=majority');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to DB')
 })
+
+// mongoose.connection.once('open', () => {
+//     console.log('connection established')
+// }).on('connectionError', (err) => {
+//     console.log(err);
+// })
 
 
 app.listen(PORT, () => {
